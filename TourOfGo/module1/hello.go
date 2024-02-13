4a2ab0f0d78508253170ca48a91811d1e7f787a4
zteanes <zteanes1@catamount.wcu.edu>
2024-02-13 12:59:51 -0500

    refactored formatting and began learning go

---
+++ TourOfGo/module1/hello.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// Go is a compiled language not interpreted
+// - static typing and run-time efficiency similar to C
+// - readability and usability similar to python
+// - meant for high-performance networking and multiprocessing
+// can use parentheses to add multiple imports
+
+// Main function that prints Hello world
+func main() {
+	fmt.Println("Hello world!")
+
+	fmt.Println("The current time is: ", time.Now())
+}
